ee7da5f22407d76777a79343bb5740b304dd1786
zfh521 <mefly@qq.com>
2024-10-21 07:41:43 +0800

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
-let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
+let userID = '72c60811-3643-4b0c-845a-8ab8dba979ae';
 
 const proxyIPs = ['cdn.xn--b6gac.eu.org:443', 'cdn-all.xn--b6gac.eu.org:443'];
 
